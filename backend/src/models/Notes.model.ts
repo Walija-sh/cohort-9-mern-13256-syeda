6e1cf3449ad927cc0e3e0ed1941513cb28d8550a
Walija-sh <walijash@gmail.com>
2026-08-04 22:38:44 +0500

    feat(notes): Added Notes model

---
+++ backend/src/models/Notes.model.ts
@@ -0,0 +1,53 @@
+import mongoose , {Document, Schema} from "mongoose";
+
+
+export interface IProseMirrorDocument{
+    type:string,
+    content?:unknown[]
+}
+
+export interface INotes extends Document{
+    title:string,
+    content: IProseMirrorDocument,
+    owner: mongoose.Types.ObjectId
+}
+
+const notesSchema=new Schema<INotes>({
+title:{
+    type:String,
+    required:[true,'Notes title is required'],
+    trim:true,
+    maxlength:[200,'Title cannot exceed 200 characters']
+},
+content:{
+    type: Schema.Types.Mixed,
+    default:{
+    type: "doc",
+    content: [],},
+    validate:{
+        validator:function (value: unknown): boolean {
+    if (typeof value !== "object" || value === null) return false;
+
+    const doc = value as IProseMirrorDocument;
+
+    if (doc.type !== "doc") return false;
+    if (!Array.isArray(doc.content)) return false;
+
+    return true;
+},
+        message: "Invalid ProseMirror document"
+
+    }
+},
+owner:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+    required:[true,'Notes must have owner']
+}
+},{
+    timestamps:true
+})
+
+const Note= (mongoose.models.Note as mongoose.Model<INotes>) || mongoose.model<INotes>('Note',notesSchema);
+
+export default Note;
