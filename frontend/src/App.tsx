3292d1a5ba83e1e806a8dda65f9baba1777f3561
Walija-sh <walijash@gmail.com>
2026-08-17 23:16:20 +0500

    chore(frontend): configure application routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,9 @@
-import { Button } from './components/ui/button'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
   return (
-  <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Notes App
-      </h1>
-      <Button>Create note</Button>
-    </div>
+  <AppRoutes/>
   )
 }
 
