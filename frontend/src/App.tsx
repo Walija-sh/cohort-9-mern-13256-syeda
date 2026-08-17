4cac5fd9e53a6b9dc00c4adfdd7915d8ff5444d2
Walija-sh <walijash@gmail.com>
2026-08-17 22:45:43 +0500

    chore(frontend): setup frontend foundation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+  <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Notes App
+      </h1>
+      <Button>Create note</Button>
+    </div>
+  )
+}
+
+export default App
